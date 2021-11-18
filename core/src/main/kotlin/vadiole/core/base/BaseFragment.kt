07b4599b0dae6b3a238efc9825d1714b20cfbd7a
vadiole <xdmitree@gmail.com>
2021-11-18 20:11:35 +0200

    add enter-exit transitions, onBack, navigation into fragments

--- core/src/main/kotlin/vadiole/core/base/BaseFragment.kt
+++ core/src/main/kotlin/vadiole/core/base/BaseFragment.kt
@@ -1,11 +1,19 @@
 package vadiole.core.base
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.transition.Fade
+import androidx.transition.Slide
+import androidx.transition.TransitionSet
 import androidx.viewbinding.ViewBinding
+import vadiole.core.ui.Scale
 
 abstract class BaseFragment<T : BaseViewModel, V : ViewBinding> : Fragment() {
 
@@ -14,15 +22,38 @@ abstract class BaseFragment<T : BaseViewModel, V : ViewBinding> : Fragment() {
     private var _binding: V? = null
     val binding: V get() = _binding!!
 
+    private var _navigator: Navigator? = null
+    val navigator: Navigator get() = _navigator!!
+
+    private val backCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            isEnabled = onBackPressed()
+            if (!isEnabled) requireActivity().onBackPressed()
+        }
+    }
+
     abstract fun onCreateBinding(inflater: LayoutInflater): V
+
+    open fun onBackPressed(): Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = onCreateBinding(inflater)
+        enterTransition = Slide(Gravity.END).setInterpolator(FastOutSlowInInterpolator())
+        exitTransition = TransitionSet()
+            .addTransition(Scale().addTarget(binding.root)).setInterpolator(FastOutSlowInInterpolator())
+            .addTransition(Fade().addTarget(binding.root)).setInterpolator(FastOutSlowInInterpolator())
+
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        _navigator = context as Navigator
+        requireActivity().onBackPressedDispatcher.addCallback(this, backCallback)
     }
 }
