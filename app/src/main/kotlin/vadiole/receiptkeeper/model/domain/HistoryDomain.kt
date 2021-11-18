9ad5c28568175054e67daaf14a8b94b3d2adc049
vadiole <xdmitree@gmail.com>
2021-11-18 10:40:39 +0200

    add models

---
+++ app/src/main/kotlin/vadiole/receiptkeeper/model/domain/HistoryDomain.kt
@@ -0,0 +1,13 @@
+package vadiole.receiptkeeper.model.domain
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+
+sealed class HistoryDomain {
+    class Receipt(val id: Int, val title: String, val datetime: LocalDateTime, raw: String) : HistoryDomain() {
+        val date: LocalDate get() = datetime.toLocalDate()
+    }
+
+    class Date(val localDate: LocalDate) : HistoryDomain()
+}
