25de7cbfd5e7bfb86fd038486f2beed91e63def8
vadiole <xdmitree@gmail.com>
2021-11-18 20:13:08 +0200

    add ScannerFragment

---
+++ app/src/main/kotlin/vadiole/receiptkeeper/ui/scanner/ScannerViewModel.kt
@@ -0,0 +1,8 @@
+package vadiole.receiptkeeper.ui.scanner
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import vadiole.core.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ScannerViewModel @Inject constructor() : BaseViewModel()
