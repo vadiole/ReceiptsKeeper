8985393f8fbe78a26530879b7258302648d09de5
vadiole <xdmitree@gmail.com>
2021-11-18 13:14:25 +0200

    add ReceiptDao

---
+++ app/src/main/kotlin/vadiole/receiptkeeper/data/local/ReceiptDao.kt
@@ -0,0 +1,23 @@
+package vadiole.receiptkeeper.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import vadiole.receiptkeeper.model.raw.ReceiptEntity
+
+@Dao
+interface ReceiptDao {
+    @Query("SELECT * FROM Receipt")
+    fun getReceipts(): Flow<List<ReceiptEntity>>
+
+    @Query("SELECT * FROM Receipt WHERE id = :id")
+    suspend fun getReceipt(id: String): ReceiptEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertReceipt(entity: ReceiptEntity)
+
+    @Query("DELETE FROM Receipt WHERE id = :id")
+    suspend fun deleteReceipt(id: Int)
+}
