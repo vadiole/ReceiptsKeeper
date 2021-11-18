f0771d355e3defa3a958d2957f1b0b84cddc7dd7
vadiole <xdmitree@gmail.com>
2021-11-18 10:39:05 +0200

    add new extensions

---
+++ core/src/main/kotlin/vadiole/core/extensions/FlowKtx.kt
@@ -0,0 +1,23 @@
+package vadiole.core.extensions
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.withContext
+
+fun <T> Flow<T>.observe(
+    owner: LifecycleOwner,
+    action: suspend (T) -> Unit,
+): Job = flowWithLifecycle(owner.lifecycle, Lifecycle.State.STARTED)
+    .onEach {
+        withContext(Dispatchers.Main) {
+            action.invoke(it)
+        }
+    }
+    .launchIn(owner.lifecycleScope)
--- core/src/main/kotlin/vadiole/core/extensions/UiKtx.kt
+++ core/src/main/kotlin/vadiole/core/extensions/UiKtx.kt
@@ -1,6 +1,11 @@
 package vadiole.core.extensions
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.RippleDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import android.view.ViewGroup
 import kotlin.math.ceil
 
@@ -14,3 +19,8 @@ fun Float.dpf(context: Context): Float = this * context.resources.displayMetrics
 fun Int.dp(context: Context): Int = ceil(this * context.resources.displayMetrics.density).toInt()
 
 fun Int.dpf(context: Context): Float = this * context.resources.displayMetrics.density
+
+fun RippleRect(color: Int, content: Drawable? = null): RippleDrawable {
+    val colorStateList = ColorStateList.valueOf(color)
+    return RippleDrawable(colorStateList, content, ShapeDrawable(RectShape()))
+}
