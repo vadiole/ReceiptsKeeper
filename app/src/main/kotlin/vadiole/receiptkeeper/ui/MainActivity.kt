dbe1fd28c121cbfaf189366b7688f9f245732fbe
vadiole <xdmitree@gmail.com>
2021-11-18 20:09:17 +0200

    add navigator, fix saving state

--- app/src/main/kotlin/vadiole/receiptkeeper/ui/MainActivity.kt
+++ app/src/main/kotlin/vadiole/receiptkeeper/ui/MainActivity.kt
@@ -1,27 +1,44 @@
 package vadiole.receiptkeeper.ui
 
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
 import vadiole.core.base.BaseActivity
+import vadiole.receiptkeeper.R
 import vadiole.receiptkeeper.ui.history.HistoryFragment
+import vadiole.receiptkeeper.ui.scanner.ScannerFragment
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val fragmentContainerId = View.generateViewId()
-        val fragmentContainer = FragmentContainerView(this).apply {
-            id = fragmentContainerId
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container, HistoryFragment(), HISTORY_FRAGMENT)
+            }
         }
-        setContentView(fragmentContainer)
+    }
+
+    override fun navigate(destination: String, args: Bundle?) {
+        if (supportFragmentManager.findFragmentByTag(destination)?.isAdded == true) return
+
+        val fragment = when (destination) {
+            SCANNER_FRAGMENT -> ScannerFragment()
+            else -> return
+        }
 
         supportFragmentManager.commit {
-            add(fragmentContainerId, HistoryFragment())
+            addToBackStack(destination)
+            replace(R.id.fragment_container, fragment, destination)
         }
+    }
+
+    companion object {
+        const val HISTORY_FRAGMENT = "history_fragment"
+        const val SCANNER_FRAGMENT = "scanner_fragment"
     }
 }
