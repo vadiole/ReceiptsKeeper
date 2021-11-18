3fe87611e89719eb5f93cd9754a4d2948633defe
vadiole <xdmitree@gmail.com>
2021-11-18 13:17:46 +0200

    add receipt repository & mapper

---
+++ app/src/main/kotlin/vadiole/receiptkeeper/mapper/ReceiptMapper.kt
@@ -0,0 +1,14 @@
+package vadiole.receiptkeeper.mapper
+
+import vadiole.receiptkeeper.model.domain.HistoryDomain
+import vadiole.receiptkeeper.model.raw.ReceiptEntity
+import javax.inject.Inject
+
+class ReceiptMapper @Inject constructor() {
+    fun map(entity: ReceiptEntity) = HistoryDomain.Receipt(
+        entity.id,
+        entity.title,
+        entity.datetime,
+        entity.raw
+    )
+}
