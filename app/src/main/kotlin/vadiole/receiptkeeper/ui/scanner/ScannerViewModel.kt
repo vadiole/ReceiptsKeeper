53cf3ef2da75aeedb1aa3f36cc24c2afd16a3768
vadiole <xdmitree@gmail.com>
2021-11-20 02:28:48 +0200

    add saving receipts

--- app/src/main/kotlin/vadiole/receiptkeeper/ui/scanner/ScannerViewModel.kt
+++ app/src/main/kotlin/vadiole/receiptkeeper/ui/scanner/ScannerViewModel.kt
@@ -1,8 +1,32 @@
 package vadiole.receiptkeeper.ui.scanner
 
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 import vadiole.core.base.BaseViewModel
+import vadiole.receiptkeeper.model.raw.ReceiptRaw
+import vadiole.receiptkeeper.usecase.SaveReceiptUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class ScannerViewModel @Inject constructor() : BaseViewModel()
+class ScannerViewModel @Inject constructor(
+    private val saveUseCase: SaveReceiptUseCase,
+) : BaseViewModel() {
+
+    private val _result = MutableStateFlow(Result.success<String?>(null))
+    val result = _result.shareIn(viewModelScope, SharingStarted.Lazily, 0)
+
+    fun saveReceipt(raw: ReceiptRaw, id: String, date: String) = viewModelScope.launch {
+        saveUseCase.invoke(raw, id, date).fold(
+            onSuccess = {
+                _result.value = Result.success(id)
+
+            }, onFailure = {
+                _result.value = Result.failure(it)
+            }
+        )
+    }
+}
